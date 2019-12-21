17dca48486b64f6d81b8b10b3a24b2a73ce3ba28
Howard Daniels <hdaniels13tat@gmail.com>
2019-12-21 12:51:27 -0500

    added stock info from three major stocks to the home page, including color coded stock value changes

--- assets/javascript/common.js
+++ assets/javascript/common.js
@@ -7,24 +7,57 @@
 
 
         console.log(response);
+        $("#amazon").text(response.data[0].name + ": ");
+        $("#amzndaychange").text(response.data[0].day_change)
+        if(response.data[0].day_change < 0){
+            $("#amzndaychange").attr("style", "color: red;");
+        }
+        else if (response.data[0].day_change == 0){
+            $("#amzndaychange").attr("style", "color: blue;");
+        }
+        else if (response.data[0].day_change > 0){
+            $("#amzndaychange").attr("style", "color: green");
+        }
   });
 
-  var queryURL = "https://api.worldtradingdata.com/api/v1/stock?symbol=" + "GOOGL" + "&api_token=if5qo5SkSbR7QWK2jE4QlqXXFzkkiM47AVu0nQQfDQLswL1k3d55gHhjZFej";
+  var queryURL2 = "https://api.worldtradingdata.com/api/v1/stock?symbol=" + "GOOGL" + "&api_token=if5qo5SkSbR7QWK2jE4QlqXXFzkkiM47AVu0nQQfDQLswL1k3d55gHhjZFej";
       $.ajax({
-        url: queryURL,
+        url: queryURL2,
         method: "GET"
       }).then(function(response) {
 
 
         console.log(response);
+        $("#google").text(response.data[0].name + ": ");
+        $("#googldaychange").text(response.data[0].day_change)
+        if(response.data[0].day_change < 0){
+            $("#googldaychange").attr("style", "color: red;");
+        }
+        else if (response.data[0].day_change == 0){
+            $("#googldaychange").attr("style", "color: blue;");
+        }
+        else if (response.data[0].day_change > 0){
+            $("#googldaychange").attr("style", "color: green");
+        }
   });
 
-  var queryURL = "https://api.worldtradingdata.com/api/v1/stock?symbol=" + "AAPL" + "&api_token=if5qo5SkSbR7QWK2jE4QlqXXFzkkiM47AVu0nQQfDQLswL1k3d55gHhjZFej";
+  var queryURL3 = "https://api.worldtradingdata.com/api/v1/stock?symbol=" + "AAPL" + "&api_token=if5qo5SkSbR7QWK2jE4QlqXXFzkkiM47AVu0nQQfDQLswL1k3d55gHhjZFej";
       $.ajax({
-        url: queryURL,
+        url: queryURL3,
         method: "GET"
       }).then(function(response) {
 
 
         console.log(response);
+        $("#apple").text(response.data[0].name + ": ");
+        $("#aapldaychange").text(response.data[0].day_change)
+        if(response.data[0].day_change < 0){
+            $("#aapldaychange").attr("style", "color: red;");
+        }
+        else if (response.data[0].day_change == 0){
+            $("#aapldaychange").attr("style", "color: blue;");
+        }
+        else if (response.data[0].day_change > 0){
+            $("#aapldaychange").attr("style", "color: green");
+        }
   });
