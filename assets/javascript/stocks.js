2933726adf518a8404b8c5dc9d0b07df3e299445
Eric Sargeant <ewsargeant@gmail.com>
2020-01-04 10:22:54 -0500

    stock page jquery

--- assets/javascript/stocks.js
+++ assets/javascript/stocks.js
@@ -5,14 +5,41 @@ function stockFinder(stock) {
       $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function(response) {
+      }).then(function(response) {      
+
+        var name = response.data[0].name;
+        var symbol = response.data[0].symbol;
+        var price = response.data[0].price;
+        var dayHigh = response.data[0].day_high;
+        var dayLow = response.data[0].day_low;
+        var dayChange = response.data[0].day_change;
+        var changepct = response.data[0].change_pct;
+        var yesterday = response.data[0].close_yesterday;
+        var marketCap = response.data[0].market_cap;
+        var volume = response.data[0].volume;
+        var pe = response.data[0].pe;
+        var eps = response.data[0].eps;
+
+        
+        $(".results").html(name);
+        var tBody = $("tbody");
+        var tRow = $("<tr>");
+     
+      var titleTd = $("<td>").text(symbol);
+      var priceTd = $("<td>").text(price);
+      var changeTd = $("<td>").text(dayChange);
+      // Append the newly created table data to the table row
+      tRow.append(titleTd, priceTd, changeTd);
+      // Append the table row to the table body
+      tBody.append(tRow);
 
 
         console.log(response);
+        console.log(name)
+        console.log(dayLow)
   })};
 
-  $("#searchIcon").on("click", function(event) {
-    alert("hiii")
+  $(".searchBtn").on("click", function(event) {
       event.preventDefault();
       var inputArtist = $("#prompt").val().trim();
   
@@ -22,10 +49,18 @@ function stockFinder(stock) {
 
     document.querySelector('#prompt').addEventListener('keypress', function (e) {
       if (e.key === 'Enter') {
-        event.preventDefault();
+      event.preventDefault();
       var inputArtist = $("#prompt").val().trim();
+      $(".stocks").remove();
+      event.preventDefault();
   
      
       stockFinder(inputArtist);
       }
      });
+
+     $(".searchBtn").on("click", function(event) {
+      $(".stocks").remove();
+      event.preventDefault();
+      
+  });
